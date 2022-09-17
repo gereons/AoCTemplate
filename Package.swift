d355508902b006cb97df75ad48da24ce90ec7054
Gereon Steffens <gereon@steffens.org>
2022-09-17 12:35:01 +0200

    move files

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -22,9 +22,11 @@ let package = Package(
             dependencies: [
                 // .product(name: "Algorithms", package: "swift-algorithms"),
                 .product(name: "AoCTools", package: "AoCTools")
-            ]),
+            ],
+            path: "Sources"),
         .testTarget(
             name: "aocTests",
-            dependencies: [ "AdventOfCode" ])
+            dependencies: [ "AdventOfCode" ],
+            path: "Tests")
     ]
 )
