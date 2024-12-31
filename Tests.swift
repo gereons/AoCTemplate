cf3d00ff298e3a3e38244eea79fb57674de3fdc7
Gereon Steffens <github@steffens.org>
2024-12-31 14:45:29 +0100

    fix async #expect

--- Tests.swift
+++ Tests.swift
@@ -13,24 +13,24 @@ struct Day{DAY}Tests {
     @Test("Day {DAY1} Part 1", .tags(.testInput))
     func testDay{DAY}_part1() async {
         let day = Day{DAY}(input: testInput)
-        #expect(await day.part1() == 0)
+        await #expect(day.part1() == 0)
     }
 
     @Test("Day {DAY1} Part 1 Solution")
     func testDay{DAY}_part1_solution() async {
         let day = Day{DAY}(input: Day{DAY}.input)
-        #expect(await day.part1() == 0)
+        await #expect(day.part1() == 0)
     }
 
     @Test("Day {DAY1} Part 2", .tags(.testInput))
     func testDay{DAY}_part2() async {
         let day = Day{DAY}(input: testInput)
-        #expect(await day.part2() == 0)
+        await #expect(day.part2() == 0)
     }
 
     @Test("Day {DAY1} Part 2 Solution")
     func testDay{DAY}_part2_solution() async {
         let day = Day{DAY}(input: Day{DAY}.input)
-        #expect(await day.part2() == 0)
+        await #expect(day.part2() == 0)
     }
 }
